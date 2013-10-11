a07cefc00a082db65eb91f42a2b1d51c466a22da
orekyuu <orekyuu@yahoo.co.jp>
2013-10-11 16:16:48 +0900

    不必要なメソッドを削除

--- JavatterFX/src/com/github/orekyuu/javatterfx/util/TwitterUtil.java
+++ JavatterFX/src/com/github/orekyuu/javatterfx/util/TwitterUtil.java
@@ -1,9 +1,6 @@
 package com.github.orekyuu.javatterfx.util;
 
-import java.io.File;
-
 import twitter4j.Status;
-import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
@@ -12,41 +9,7 @@ import twitter4j.TwitterException;
  * @author orekyuu
  *
  */
-public class TwitterUtil {
-
-	private long replyID;
-	private File file;
-
-	/**
-	 * 設定されたデータを使ってつぶやきます
-	 * @param twitter Twitterインスタンス
-	 * @param message つぶやく内容
-	 * @throws TwitterException
-	 */
-	public void tweet(final Twitter twitter,final String message) throws TwitterException{
-		if(message.length()==0)return;
-		Thread th=new Thread(){
-			@Override
-			public void run(){
-				try {
-					StatusUpdate update=new StatusUpdate(message);
-					if(replyID!=-1){
-						update.setInReplyToStatusId(replyID);
-						replyID=-1;
-					}
-
-					if(file!=null){
-						update.media(file);
-						file=null;
-					}
-					twitter.updateStatus(update);
-				} catch (TwitterException e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		th.start();
-	}
+public final class TwitterUtil {
 
 	/**
 	 * 指定されたStatusをリツイートします
@@ -79,14 +42,6 @@ public class TwitterUtil {
 	}
 
 	/**
-	 * リプライ先を設定
-	 * @param status リプライ先のつぶやき
-	 */
-	public void setReplyID(Status status){
-		replyID = status.getId();
-	}
-
-	/**
 	 * お気に入りに追加します
 	 * @param twitter Twitterインスタンス
 	 * @param status お気に入りに登録するつぶやき
@@ -103,14 +58,6 @@ public class TwitterUtil {
 			}
 		};
 		th.start();
-	}
-
-	/**
-	 * つぶやきに画像を添付する
-	 * @param image 添付したい画像のパス
-	 */
-	public void setImage(File image){
-		file=image;
 	}
 
 	/**
